3458e597dc110d7df90d91c3366f4bd9ad8ef9c6
johnros <johnros@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-12-27 15:59:58 +0000

    Rerewitten estimate.rds3 after is was missing in revision 14. Did not package.skeleton and roxugen yet.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rds2@15 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/build/onlyFunctions.R
+++ pkg/build/onlyFunctions.R
@@ -79,7 +79,7 @@ qnorm.theta<- function(theta, const, range=20, minimum=-10){
 ## Test:
 # inv.qnorm.theta(10, 100)
 # inv.qnorm.theta(10, 10)
-#inv.qnorm.theta(20000,10)
+# inv.qnorm.theta(20000,10)
 # inv.qnorm.theta(25,200)
 # inv.qnorm.theta(25,200, range=2)
 # inv.qnorm.theta(25,200, range=1)
@@ -122,110 +122,7 @@ compute.S<- function(Sij){
 }
 
 
-
-
-# Optimization stage:
-
 ## TODO: Fix estimation for a single degree!
-
-# Note: make sure likelihood.cpp is compiled for the right system!
-# For 32 bit machines: dyn.load("/home/johnros/workspace/rds2/pkg/likelihoodVer9.32.so")
-# dyn.load("/home/johnros/workspace/rds2/pkg/likelihoodVer9.64.so")
-
-
-
-
-#' Main function in rds2 package. Returns the ML estimate of population size and degree distribution. 
-#' @param func 
-#' @param data 
-#' @param init 
-#' @param const 
-#' @param arc 
-#' @param maxit 
-#' @param theta.range 
-#' @param theta.minimum 
-#' @param ... 
-#' @return TBC 
-#' @author johnros
-#' @export
-#' @useDynLib rds2
-#' @examples 
-#' data(simulation)
-#' estimate.rds(unlist(data3[1,]), const=20, arc=FALSE)
-estimate.rds<- function (data, init, const, arc=FALSE, maxit=1000,...) {
-	# Look for degrees in the data, so their estimates are nony vanishing
-	N.j<- rep(0, max(data)) 
-	uniques<- unique(data)
-	uniques<- uniques[uniques!=0]
-	s.uniques<- sort(uniques)
-	param.size<- length(uniques)
-	data.table<-table(data)[-1]
-	
-	likelihood.wrap1<- function(par){
-		final.result<- -Inf
-		ccc<- exp(par[1])
-		theta<- inv.qnorm.theta(par[2], const = const)
-		N.j<- rep(0, max(data))
-		N.j[s.uniques]<- exp(tail(par,-2))
-		if(any( N.j[s.uniques] < data.table )) return(final.result)	  
-		
-		if(is.numeric(ccc) && is.numeric(theta) && !is.infinite(theta) && !is.infinite(ccc) ) {
-			result<- .C("likelihood", 
-					sample=as.integer(data), 
-					c=as.numeric(ccc), 
-					theta=as.numeric(theta), 
-					Nj=as.numeric(N.j), 
-					constant=as.numeric(const),
-					n=as.integer(length(data)), 
-					N=as.integer(length(N.j)),
-					arc=arc,
-					result=as.double(0))
-			final.result<- result$result
-		}	  
-		#print(c(as.integer(data),log.c=as.numeric(par[1]),qnorm.theta=as.numeric(par[2]),Nj=as.numeric(N.j),constant=as.numeric(const)))
-		
-		return(final.result)
-	}
-	cap<- max(data)
-	cap.arcs<-  sum.ranks(data)
-	max.cap.arcs<- max(cap.arcs)
-	log.c<- -log(max(sum.ranks(data)*sum(data)) * max(data)) -1
-	
-	if(missing(init)) { 
-		init<- list( 
-				#one= c(log.c=log.c, qnorm.theta=qnorm.theta(1,const), log(rep(10*cap, param.size))+1),
-				#two= c(log.c=log.c, qnorm.theta=qnorm.theta(1,const), log(rep(initial.Nj,param.size))+1),
-				#three= c(log.c=log.c, qnorm.theta=qnorm.theta(1,const), log(runif(min=cap, max=3*cap ,n=param.size))+1) ,
-				#four= c(log.c=log.c, qnorm.theta=qnorm.theta(1,const), log(cap.arcs)+1),
-				#five= c(log.c=log.c, qnorm.theta=qnorm.theta(1,const), log(rep(max.cap.arcs, param.size))+1 ),
-				six0.2= c(log.c=log.c, qnorm.theta=qnorm.theta(0.2,const), log(data.table)+1 ),
-				#six0.5= c(log.c=log.c, qnorm.theta=qnorm.theta(0.5,const), log(data.table)+1 ),
-				#six0.9= c(log.c=log.c, qnorm.theta=qnorm.theta(0.9,const), log(data.table)+1 ),
-				six1= c(log.c=log.c, qnorm.theta=qnorm.theta(1,const), log(data.table)+1 ),
-				#six1.1= c(log.c=log.c, qnorm.theta=qnorm.theta(1.1,const), log(data.table)+1 ),
-				#six1.5= c(log.c=log.c, qnorm.theta=qnorm.theta(1.5,const), log(data.table)+1 ),
-				six2= c(log.c=log.c, qnorm.theta=qnorm.theta(2,const), log(data.table)+1 )			  
-		)}
-	
-	likelihood.optim<-lapply(init, function(x) {
-				try(optim(par=x, fn=likelihood.wrap1, control=list(fnscale=-1, maxit=maxit),...)) }  )  
-	
-	prepare.result<- function(x){    
-		N.j[s.uniques]<- exp(tail(x$par,-2))
-		result<- list( 
-				c=exp(x$par[[1]]), 
-				theta=inv.qnorm.theta(x$par[[2]],const), 
-				Nj=N.j, 
-				x  )
-		return(result)
-	}  
-	return(lapply(likelihood.optim, function(x) try(prepare.result(x))))
-}
-
-
-
-
-
 
 #' Same as estimate.rds, but for a fixed theta.
 #' @param data 
@@ -238,6 +135,7 @@ estimate.rds<- function (data, init, const, arc=FALSE, maxit=1000,...) {
 #' @param ... 
 #' @return TBC 
 #' @author johnros
+#' @useDynLib rds2
 #' @export 
 #' @examples
 #' data(brazil)
@@ -305,5 +203,104 @@ estimate.rds2<- function (data, Sij, init, const, arc=FALSE, maxit=10000, theta,
 	}  
 	return(lapply(likelihood.optim, function(x) try(prepare.result(x))))
 }
+
+
+
+#' Main function in rds2 package. Returns the ML estimate of population size and degree distribution.
+#' 
+#' Performs maximum likelihood estimation of the population size, degree distribution and theta [explain] allowing (a) "soft" constraints on theta and (b) an input Sij matrix. 
+#' 
+#' @param data 
+#' @param Sij 
+#' @param init 
+#' @param const 
+#' @param arc 
+#' @param maxit 
+#' @param initial.thetas 
+#' @param theta.minimum 
+#' @param theta.range 
+#' @param ... 
+#' @author Jonathan Rosenblatt
+#' @useDynLib rds2
+#' @export
+#' data(simulation)
+#' estimate.rds3(temp.data, Sij = make.Sij(temp.data), initial.thetas = c(1,10), arc = FALSE, maxit = 1000, const = 0.5, theta.minimum = -0.5, theta.range = 2)
+estimate.rds3<- function (data, Sij, init, const, arc=FALSE, maxit=10000, initial.thetas, theta.minimum, theta.range, ...) {
+	# Look for degrees in the data, so their estimates are non vanishing
+	N.j<- rep(0, max(data)) 
+	uniques<- unique(data)
+	uniques<- uniques[uniques!=0]
+	sorted.uniques<- sort(uniques)
+	param.size<- length(uniques)
+	data.table<-table(data)[-1]
+	
+	# Compute the size of the snowball along the sample:
+	S<- compute.S(Sij)
+	
+	
+	# Wrapper to the likelihood function. Implements constraints on parameters by taking real valued parameters and remapping them.
+	likelihood.wrap<- function(par){
+		final.result<- -Inf # initialize output
+		beta<- exp(par[1])
+		theta<- inv.qnorm.theta(par[2], const = const)		
+		## TODO: A) Add estimatino of theta. 
+		N.j<- rep(0, max(data))
+		N.j[sorted.uniques]<- exp(tail(par,-2)) # fill non trivial Nj estimates.
+		if(any( N.j[sorted.uniques] < data.table )) return(final.result) # checks that given Njs correspond to estimatable values.		
+		
+		if(is.numeric(beta) && is.numeric(theta) && !is.infinite(theta) && !is.infinite(beta) ) {
+			result<- .C("likelihood", 
+					sample=as.integer(data), 
+					Sij=as.integer(as.matrix(Sij)),
+					S=as.integer(S),				  
+					c=as.numeric(beta), 
+					theta=as.numeric(theta), 
+					Nj=as.numeric(N.j), 
+					constant=as.numeric(const),
+					observed_degrees=as.integer(rownames(Sij)),
+					n=as.integer(length(data)), 
+					N=as.integer(length(N.j)),
+					N_observed=as.integer(nrow(Sij)),				  
+					arc=arc,
+					result=as.double(0))		  
+			final.result<- result$result
+		}	  	  
+		return(final.result)
+	}
+	
+	## Initialize estimation:
+	
+	cap<- max(data)
+	cap.arcs<-  sum.ranks(data)
+	max.cap.arcs<- max(cap.arcs)
+	log.beta<- -log(max(sum.ranks(data)*sum(data)) * max(data)) - 0.01 # beta has to be such that all probabilities in the likelihood are between 0 and 1. In particular, for the last subject sampled.	
+	
+	
+	if(missing(init)) {		
+		init<- lapply(initial.thetas, function(x) c(log.c=log.beta, qnorm.theta=qnorm.theta(x,const), log(data.table)+1 ))
+	}
+	
+	likelihood.optim<-lapply(init, function(x) {
+				try(optim(par=x, fn=likelihood.wrap, control=list(fnscale=-1, maxit=maxit),...)) 
+			}  ) 
+	
+	prepare.result<- function(x){
+		result<- simpleError("Optim did not converge") 
+		if(is.list(x)){
+			N.j[sorted.uniques]<- exp(tail(x$par,-2))
+			result<- list( 
+					c=exp(x$par[[1]]), 
+					theta=inv.qnorm.theta(x$par[[2]], const=const, range=theta.range, minimum=theta.minimum), 
+					Nj=N.j,
+					iterations=x$counts,
+					likelihood.optimum=x$value)			
+		}		
+		return(result)
+	} 
+	## TODO: A) Return only numeric objects
+	
+	return(lapply(likelihood.optim, prepare.result))							
+}
+
 
 
