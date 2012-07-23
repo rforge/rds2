53c3babb3fc3eb76ab0ce4be9825b7b6f994535a
johnros <johnros@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-07-23 19:38:57 +0000

    nothing special
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rds2@31 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/rds2/R/simulator.R
+++ pkg/rds2/R/simulator.R
@@ -11,49 +11,66 @@ head(incidence.list)
 
 
 #### Generate a sample under the RDS assumption ####
-sample.length<- 10000
+sample.length<- 2000
 theta<- 1
-beta<- 1e-10
 Njs<- table(unlist(neighbour.count))
-  
-sampled<- rep(FALSE, network.size)
-sampled[sample(seq(1, network.size),size=1)]<- TRUE
-sampled.degrees<- rep(0, sample.length)
+N<- network.size
+js<- as.numeric(names(Njs))
+
+(maximal.beta<- 1/ (N * max(Njs) * max(js) ))
+beta<- maximal.beta*20
+
+Uik<- rep(0, length.out=max(js))
+names(Uik)<- seq(along.with=Uik)
+
+snowball<- 1
 for(i in seq(1, sample.length)){
-  # compute the probability of each individual being sampled  
-  sample.indexes<- seq(1, network.size)[!sampled]
-  snowball<- sum(sampled)
-  sampled.table<- table(unlist(neighbour.count[sampled]))  
-  probs.function<- function(k) {
-    # How many subject with degree k have been sampled?
-    Uik<- sampled.table[paste(k)]
-    Uik<- ifelse(is.na(Uik), 0, Uik)
-    # How many subject with degree k are out there?
-    Sik<- Njs[paste(k)]-Uik
-    return(beta * snowball * Sik * k^theta )
-  }  
-  probs.vector<- sapply(neighbour.count[!sampled], probs.function)
-  
-  # should someone be sampled?
-  someone.sampled<- as.logical(rbinom(n=1, size=1, prob=1-sum(probs.vector)))  
-    # sample with those probabilities
-  if(length(sample.indexes)>0 && someone.sampled){
-    if(length(sample.indexes)==1) next.sample<- sample.indexes
-    else next.sample<- sample(sample.indexes, 1, prob=probs.vector)
-    
-    sampled[next.sample]<- TRUE
-    sampled.degrees[i]<- neighbour.count[[next.sample]]    
-    sample.indexes<- seq(1, network.size)[!sampled]    
+  # i<- 10
+  # compute the probability of degree 0.
+  pi.function<- function(k) {
+    beta * k^theta * snowball * (Njs[paste(k)]-Uik[k] )    
   }
-  else {
-    sampled.degrees[i]<- 0
-  }  
+  
+  probs.function<- function(k){
+    pi.function(k) * prod(sapply(js[js!=k], pi.function  ))
+  }
+  
+  (no.sample.probability<- 1 - sum(sapply(js, probs.function)))
+  
+  
+  
+  # if someone is sampled:
+  sample.someon<- as.logical(rbinom(1, 1, prob=no.sample.probability ))
+  if(sample.someone){
+    Uik[]
+    snowball<- snowball+1
+  }
+  # compute the probability of each degree    
+  # update the table of degree counts (Njs)
+  snowball<- sum(Uik)  
 }
 tail(sampled.degrees)
+head(sampled.degrees)
 
+
+#### Estimation ####
 require(rds2)
 (estimated.simulation<- estimate.rds3(data=sampled.degrees, Sij=make.Sij(data=sampled.degrees), initial.thetas=c(0.5,1,1.5), const=0.5, theta.minimum=-2, theta.range=4))
 comparison(estimated.simulation)
 
 
 
+
+
+
+
+
+# Code from previous versions:
+probs.function<- function(k) {
+  # How many subject with degree k have been sampled?
+  Uik<- sampled.table[paste(k)]
+  Uik<- ifelse(is.na(Uik), 0, Uik)
+  # How many subject with degree k are out there?
+  Sik<- Njs[paste(k)]-Uik
+  return( snowball * Sik * k^theta )
+}
