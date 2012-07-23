639e0686e2ce94f7fc0ba6c0a1eb4ad248fa3004
johnros <johnros@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-07-23 22:31:07 +0000

    still looking for that normalizing constant
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rds2@32 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/rds2/R/simulator.R
+++ pkg/rds2/R/simulator.R
@@ -4,73 +4,47 @@ rm(list=ls())
 network.size<- 1000
 network.density<- 0.02
 neighbour.count<- lapply(seq(1, network.size), function(x) rbinom(1, network.size, network.density))
-incidence.list<- lapply(neighbour.count, function(x) sample(x=seq(1,network.size), size=x))
-head(incidence.list)
-
-
-
+# incidence.list<- lapply(neighbour.count, function(x) sample(x=seq(1,network.size), size=x))
 
 #### Generate a sample under the RDS assumption ####
-sample.length<- 2000
 theta<- 1
 Njs<- table(unlist(neighbour.count))
-N<- network.size
 js<- as.numeric(names(Njs))
-
+N<- network.size
 (maximal.beta<- 1/ (N * max(Njs) * max(js) ))
-beta<- maximal.beta*20
+(beta<- maximal.beta*200) # Note: larger beta favour sampling non 0 degrees.
+## TODO: Find the normalizing constant.
 
+sample.length<- 1000
 Uik<- rep(0, length.out=max(js))
 names(Uik)<- seq(along.with=Uik)
-
 snowball<- 1
+degree.sampled.vec<- rep(NA, sample.length)
 for(i in seq(1, sample.length)){
-  # i<- 10
-  # compute the probability of degree 0.
-  pi.function<- function(k) {
-    beta * k^theta * snowball * (Njs[paste(k)]-Uik[k] )    
-  }
-  
-  probs.function<- function(k){
-    pi.function(k) * prod(sapply(js[js!=k], pi.function  ))
-  }
-  
-  (no.sample.probability<- 1 - sum(sapply(js, probs.function)))
-  
-  
+  pi.function<- function(k) beta * k^theta * snowball * (Njs[paste(k)] - Uik[k] )     
+  probs.function<- function(k)  pi.function(k) * prod(sapply(js[js!=k], function(x) 1-pi.function(x)  ))
+  degree.probabilities<- sapply(js, probs.function)
+  (no.sample.probability<- prod(sapply(js, function(x) 1-pi.function(x) )))
+  sample.someone<- as.logical(rbinom(1, 1, prob=1-no.sample.probability ))
   
   # if someone is sampled:
-  sample.someon<- as.logical(rbinom(1, 1, prob=no.sample.probability ))
   if(sample.someone){
-    Uik[]
-    snowball<- snowball+1
+    # sample degree
+    degree.sampled<- sample(x=js, prob=degree.probabilities, size=1)      
+    # increase Uik
+    Uik[degree.sampled]<- Uik[degree.sampled]+1
+    snowball<- sum(Uik)
+    degree.sampled.vec[i]<- degree.sampled    
+  }  
+  else{
+    degree.sampled.vec[i]<- 0
   }
-  # compute the probability of each degree    
-  # update the table of degree counts (Njs)
-  snowball<- sum(Uik)  
 }
-tail(sampled.degrees)
-head(sampled.degrees)
+tail(degree.sampled.vec)
+head(degree.sampled.vec)
 
 
 #### Estimation ####
-require(rds2)
-(estimated.simulation<- estimate.rds3(data=sampled.degrees, Sij=make.Sij(data=sampled.degrees), initial.thetas=c(0.5,1,1.5), const=0.5, theta.minimum=-2, theta.range=4))
-comparison(estimated.simulation)
-
-
-
-
-
-
-
-
-# Code from previous versions:
-probs.function<- function(k) {
-  # How many subject with degree k have been sampled?
-  Uik<- sampled.table[paste(k)]
-  Uik<- ifelse(is.na(Uik), 0, Uik)
-  # How many subject with degree k are out there?
-  Sik<- Njs[paste(k)]-Uik
-  return( snowball * Sik * k^theta )
-}
+# require(rds2)
+# (estimated.simulation<- estimate.rds3(data=sampled.degrees, Sij=make.Sij(data=sampled.degrees), initial.thetas=c(0.5,1,1.5), const=0.5, theta.minimum=-2, theta.range=4))
+# comparison(estimated.simulation)
