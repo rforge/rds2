4495c00d60be9405ba59efac7e2236f78d4a1793
johnros <johnros@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-07-25 14:12:17 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rds2@36 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/rds2/R/simulator.R
+++ pkg/rds2/R/simulator.R
@@ -3,7 +3,7 @@ rm(list=ls())
 
 
 network.size<- 2000
-network.density<- 0.02
+network.density<- 0.002
 neighbour.count<- lapply(seq(1, network.size), function(x) rbinom(1, network.size, network.density))
 #incidence.list<- lapply(neighbour.count, function(x) sample(x=seq(1,network.size), size=x))
 
@@ -14,7 +14,7 @@ generate.sample <- function(theta, Njs, beta, sample.length) {
 	#Njs<- table(unlist(neighbour.count))
 	js<- as.numeric(names(Njs))
 	N<- sum(Njs)
-	maximal.beta<- 1/ (N * max(Njs) * max(js) )
+	maximal.beta<- 1/ (N * max(Njs) * max(js)^theta )
 	#beta<- maximal.beta
 	
 	error.messge<- paste("beta value too large. Errors might occur. \nFor the given degree frequencies, consider keeping it under ", round(maximal.beta,2), sep="" )
@@ -30,11 +30,12 @@ generate.sample <- function(theta, Njs, beta, sample.length) {
 		pi.function<- function(k) beta * k^theta * snowball * (Njs[paste(k)] - Uik[k] )
 		# Compute the probabilities of sampling degree k=0:
 		no.sample.probability<- prod(sapply(js, function(x) 1-pi.function(x) ))
-		sample.someone<- as.logical(rbinom(1, 1, prob=1-no.sample.probability ))		
+		sample.someone<- as.logical(rbinom(1, 1, prob = 1 - no.sample.probability ))		
 		if(sample.someone){
 			# Compute the probabilities of sampling degree k>0:
 			probs.function<- function(k)  pi.function(k) * prod(sapply(js[js!=k], function(x) 1-pi.function(x)  ))
 			degree.probabilities<- sapply(js, probs.function)
+      #capture.output(sum(degree.probabilities)+ no.sample.probability, file='Yakir.txt', append=TRUE)
 			degree.sampled<- sample(x=js, prob=degree.probabilities, size=1)      
 			Uik[degree.sampled]<- Uik[degree.sampled]+1
 			snowball<- sum(Uik)
@@ -46,10 +47,19 @@ generate.sample <- function(theta, Njs, beta, sample.length) {
 	}
 	return(degree.sampled.vec)
 }
-degree.sampled.vec<-generate.sample(theta = theta, Njs = table(unlist(neighbour.count)), beta = 2e-8, sample.length = 10000)
+
+Njs<- table(unlist(neighbour.count))
+theta<- 1
+beta<- 9e-9 #2e-8
+
+degree.sampled.vec<-generate.sample(theta = theta, Njs = Njs, beta = beta, sample.length = 10000)
+plot(degree.sampled.vec, type='h')
+sample.ind<- degree.sampled.vec>0
+sample.indexes<- seq(along.with=sample.ind)[sample.ind]
+lines(lowess(degree.sampled.vec[sample.ind]~sample.indexes), col='red')
 #save(degree.sampled.vec, file="~/Dropbox/Yakir/test.degrees.RData")
 #load(file="~/Dropbox/Yakir/test.degrees.RData")
-plot(degree.sampled.vec, type='h')
+
 
 
 #### Estimation ####
@@ -63,6 +73,10 @@ require(rds2)
 best<- estimated.simulation[[which.max(sapply(estimated.simulation, function(x) x$likelihood.optimum))]]
 plot(Njs, type='h')	
 points(best$Nj, type='h', col='red')
+
+comparison.1<- cbind(true= beta * as.numeric(names(Njs))^theta, estimated=best$c * best$Nj[as.numeric(names(Njs))]^best$theta )
+matplot(comparison.1, type='o')	
+
 
  
  
